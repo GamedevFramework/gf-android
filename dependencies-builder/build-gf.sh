3b20ab764d5bdcef110170b48e39e22f615e8a33
ahugeat <hugeat.arthur@gmail.com>
2021-06-15 22:51:53 +0200

    Add script to build gf

---
+++ dependencies-builder/build-gf.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+SOURCE_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+source "$SOURCE_DIR/targets.sh"
+source "$SOURCE_DIR/functions.sh"
+
+# Check arguments
+if [ $# -ne 1 ]
+then
+  echo "Missing arguments" >&2
+  echo "Usage:" >&2
+  echo -e "\t$0 NDK_DIR" >&2
+
+  exit 1
+fi
+NDK_DIR=$1
+GF_DIR="$SOURCE_DIR/src/gf"
+
+# Clone or update gf repository
+if [ ! -d "$GF_DIR" ]
+then
+  git clone --recursive --depth 1 --branch develop https://github.com/GamedevFramework/gf.git "$GF_DIR"
+fi
+
+for TARGET in $ARCH_TARGETS
+do
+  BUILD_DIR="$GF_DIR/build-$TARGET"
+
+  false
+  # Clean previous build
+  rm -rf "$BUILD_DIR"
+
+  # Configure the project
+  cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_SYSTEM_VERSION=24 \
+    -DCMAKE_ANDROID_ARCH_ABI=$TARGET \
+    -DCMAKE_ANDROID_NDK="$NDK_DIR" \
+    -DCMAKE_INSTALL_PREFIX="$SOURCE_DIR/out/$TARGET" \
+    -DCMAKE_FIND_ROOT_PATH="$SOURCE_DIR/out/$TARGET" \
+    -DGF_USE_EMBEDDED_LIBS=ON \
+    -DGF_BUILD_GAMES=OFF \
+    -DGF_BUILD_EXAMPLES=OFF \
+    -DGF_BUILD_DOCUMENTATION=OFF \
+    -DGF_SINGLE_COMPILTATION_UNIT=OFF \
+    -S "$GF_DIR" \
+    -B "$BUILD_DIR" || die "CMake configuration for $TARGET failed"
+
+  # Build project
+  cmake --build "$BUILD_DIR" --parallel $(nproc --all) || die "Build for $TARGET failed"
+
+  # Export libraries
+  cmake --install "$BUILD_DIR" || die "Install for $TARGET failed"
+done
